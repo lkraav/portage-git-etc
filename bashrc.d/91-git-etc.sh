e9b9d2fcd4a11fda61b55614c17240832724a338
Leho Kraav <leho@kraav.com>
2013-11-25 02:02:32 +0200

    properly use BashrcdPhase

--- bashrc.d/91-git-etc.sh
+++ bashrc.d/91-git-etc.sh
@@ -1,22 +1,29 @@
 # portage/bashrc.d/91-git-etc.sh 0.1
 
-echo "${MYPREFIX} Phase: $EBUILD_PHASE"
 ETC="${PORTAGE_CONFIGROOT%/}/etc"
 HOST="$(hostname)"
 GITCMD="GIT_DIR=$ETC/.git GIT_WORK_TREE=$ETC GIT_AUTHOR_NAME=Portage GIT_AUTHOR_EMAIL=portage@${HOST} git"
 MYPREFIX="[91]"
 
-case "$EBUILD_PHASE" in
-    "setup")
-        STATUS=$(eval $GITCMD status -uno -s)
-        [ -n "${STATUS}" ] && die "${MYPREFIX} Error: $ETC is not clean"
-    ;;
-    "postinst")
-    ;;     
-esac
+GitEtcAll() {
+    echo "${MYPREFIX} Phase: $EBUILD_PHASE"
+}
 
-post_pkg_postinst() {
+GitEtcSetup() {
+    BashrcdTrue ${ENABLE_91_GIT_ETC} || return 0
+
+    STATUS=$(eval $GITCMD status -uno -s)
+    [ -n "${STATUS}" ] && die "${MYPREFIX} Error: $ETC is not clean"
+}
+
+GitEtcPostInst() {
+    BashrcdTrue ${ENABLE_91_GIT_ETC} || return 0
+
     etc-update 0</dev/tty 1>/dev/tty || die "${MYPREFIX} Error: etc-update failed!"
     eval $GITCMD add $ETC
     eval $GITCMD commit -q -a -m \"emerge $CATEGORY/$P\"
 }
+
+BashrcdPhase all GitEtcAll
+BashrcdPhase setup GitEtcSetup
+BashrcdPhase postinst GitEtcPostInst
